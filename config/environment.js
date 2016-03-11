26154c439903a2e0dc42e587396c8d9f1ba2ec0e
Elana Kopelevich <ekopelevich@gmail.com>
2016-03-11 16:49:58 -0700

    updated apiHost to deployed version

--- config/environment.js
+++ config/environment.js
@@ -26,8 +26,8 @@ module.exports = function(environment) {
     ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     ENV.APP.LOG_VIEW_LOOKUPS = true;
     // ENV.apiHost = 'http://localhost:4200';
-    // ENV.apiHost = 'https://mysterious-harbor-73030.herokuapp.com';
-    ENV.apiHost = 'http://localhost:3000';
+    ENV.apiHost = 'https://mysterious-harbor-73030.herokuapp.com';
+    // ENV.apiHost = 'http://localhost:3000';
   }
 
   if (environment === 'test') {
