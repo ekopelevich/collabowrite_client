5e8ecf76047c028cc066a43d903023ac11114a52
Elana Kopelevich <ekopelevich@gmail.com>
2016-03-17 14:23:10 -0600

    data appearing on individual story page correctly

--- app/stories/route.js
+++ app/stories/route.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    // return this.store.findAll('story');
     return Ember.RSVP.hash({
       users: this.store.findAll('user'),
       stories: this.store.findAll('story'),
--- app/story/route.js
+++ app/story/route.js
@@ -2,12 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params){
-    return this.store.findRecord('story', params.story_id);
-
-    // Ember.RSVP.hash({
-    //   users: this.store.findAll('user'),
-    //   stories: this.store.findAll('story'),
-    //   contributions: this.store.findAll('contribution')
-    // });
+    return Ember.RSVP.hash({
+      // users: this.store.findAll('user'),
+      story: this.store.findRecord('story', params.story_id),
+      contributions: this.store.findAll('contribution')
+    });
   }
 });
